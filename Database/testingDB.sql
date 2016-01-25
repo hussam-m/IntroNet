31ed3c14ed275b9851c6023e86114c3f71a8abc0
hussam-m <hussam.am@gmail.com>
2016-01-24 20:48:46 -0500

    Update testingDB.sql

--- Database/testingDB.sql
+++ Database/testingDB.sql
@@ -1,1 +1,16 @@
-/* this SQL code is for testing */
+-- this SQL code is for testing 
+--
+-- Database: `IntroNet`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Login_Details`
+--
+
+CREATE TABLE `Login_Details` (
+  `UserName` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `IsAdmin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`UserName`)
+);
