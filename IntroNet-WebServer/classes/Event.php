104dd44806ba8eb06b1a0a709d9f96823f4e7902
RaniaSh <rsmahmood@bsu.edu>
2016-02-19 14:22:41 -0500

    creating event class

---
+++ IntroNet-WebServer/classes/Event.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Event
+ *
+ * @author raniaalkhazaali
+ */
+class Event {
+    //put your code here
+    function getName()
+    {
+        
+    }
+    function setName()
+    {
+        
+    }
+    function getDateTime()
+    {
+        
+    }
+    function setDateTime($Datetime)
+    {
+        
+    }
+    function isRegister($Participant)
+    {
+        
+    }
+    function isAttended($Participant)
+    {
+        
+    }
+    function addPoster($Poster)
+    {
+        
+    }
+    function isLeft($Participant)
+    {
+        
+    }
+    function missingParticipants()
+    {
+        
+    }
+    function allParticipants()
+    {
+        
+    }
+}
