561d9ee1727546fd2c71a954b930659fcdd42c4f
SandeepApsingekar <sandeep.apsingekar01@gmail.com>
2016-02-21 16:39:20 -0500

    Added function's inside classes

---
+++ IntroNet-WebServer/classes/Participant.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Participant
+ *
+ * @author Sandeep
+ */
+class Participant extends User {
+    //put your code here
+    function register()
+    {
+        
+    }
+    function attend()
+    {
+        
+    }
+    function leave()
+    {
+        
+    }
+    function joinTable()
+    {
+        
+    }
+    function visitPoster()
+    {
+        
+    }
+}
---
+++ IntroNet-WebServer/classes/Planner.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Planner
+ *
+ * @author Sandeep
+ */
+class Planner extends User
+{
+    //put your code here
+    function manageEvent()
+    {
+        
+    }
+}
