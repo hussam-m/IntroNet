795ad5282ac82a8a3dc9aead21d8a7255f6295d3
Chakshu07 <chakshumanrao@gmail.com>
2016-04-11 16:44:40 -0400

    Added files via upload
    Add Participant page

---
+++ IntroNet-WebServer/classes/pages/InsertParticipant.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'Page.php';
+require_once './classes/components/CustomHTML.php';
+require_once './classes/components/Message.php';
+
+//$fnameErr = $emailErr = $lnameErr = $contactnoErr = "";
+
+/**
+ * Description of newPHPClass
+ *
+ * @author hussam
+ */
+class InsertParticipant extends Page {
+    
+    public function callBack($data, $action, \PageBody &$body) {
+        $body->addToTop(new Message("Please complete all the details", Message::DANGER));
+    }
+
+    
+    protected function build(PageBody &$body, SubMenu &$submenu) {
+        $this->pageName = "Insert Participant";
+       
+        $form = new Form("Participant");
+        $form->addInput(Input::textInput("firstName","First Name"));
+    
+      
+        $form->addInput(Input::textInput("lastName","Last Name"));
+        $form->addInput(Input::textInput("email","Email Address"));
+        $form->addInput(Input::textInput("contactNo","Contact Number"));
+        
+    
+        
+    
+        $body->addToCenter($form);
+    }
+}
+
+
